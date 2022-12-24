da47de7d10360e7e3a09bff5d80d15ce74078753
0xElshazly <mo.ayman.sa2019@gmail.com>
2022-12-24 19:46:53 +0200

    Added Sticky Navbar Function in main.js File

--- js/main.js
+++ js/main.js
@@ -15,5 +15,14 @@
     // Initiate the wowjs
     new WOW().init();
     
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 45) {
+            $('.navbar').addClass('sticky-top shadow-sm');
+        } else {
+            $('.navbar').removeClass('sticky-top shadow-sm');
+        }
+    });
+
 })(jQuery);
 
