b3fff72e8666be3cab1c0449b80977d5a958bf50
0xElshazly <mo.ayman.sa2019@gmail.com>
2022-12-25 15:07:48 +0200

    Added Back To Top Button Function in main.js File

--- js/main.js
+++ js/main.js
@@ -55,5 +55,17 @@
             delay: 10,
             time: 2000
         });
+          // Back to top button
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 100) {
+                $('.back-to-top').fadeIn('slow');
+            } else {
+                $('.back-to-top').fadeOut('slow');
+            }
+        });
+        $('.back-to-top').click(function () {
+            $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+            return false;
+        });
 })(jQuery);
 
