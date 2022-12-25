b7aa7d0b94ae2bd932ce993098cfbfbc3d25cf32
0xElshazly <mo.ayman.sa2019@gmail.com>
2022-12-25 18:59:34 +0200

    Added Testimonials Carousel Function in main.js File

--- js/main.js
+++ js/main.js
@@ -67,5 +67,27 @@
             $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
             return false;
         });
+        // Testimonials carousel
+        $(".testimonial-carousel").owlCarousel({
+            autoplay: true,
+            smartSpeed: 1500,
+            dots: true,
+            loop: true,
+            center: true,
+            responsive: {
+                0:{
+                    items:1
+                },
+                576:{
+                    items:1
+                },
+                768:{
+                    items:2
+                },
+                992:{
+                    items:3
+                }
+            }
+        });
 })(jQuery);
 
